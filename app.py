03d1cf3992e0b007cf4455f2c08a28842db2e94b
kcbson11 <kobicohen22@gmail.com>
2025-09-15 09:18:21 +0300

    Add requirements.txt and fix Pylint docstrings

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
+"""Flask web app for QuakeWatch."""
+
 from flask import Flask
+
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
+    """Return a greeting message."""
     return "Hello, World!"
 
 if __name__ == "__main__":
